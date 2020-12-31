07434e84077cdb60e0c391117fde48ee6358bd2c
agerard57 <56207146+agerard57@users.noreply.github.com>
2020-12-31 05:55:56 +0100

    Made quite a few changes since last time, don't count on me to tell what they are -_-

--- js/script.js
+++ js/script.js
@@ -1,11 +1,47 @@
+var handlerFired;
+window.addEventListener('scroll', function (e)
+{
+    var containerTop = document.querySelector('.presentation').getBoundingClientRect().top;
+    if (containerTop <= 0)
+    {
+        if (!handlerFired)
+        {
+            handlerFired = 1;
+            console.log('container at top of viewport or above');
+            document.getElementById("logo_animation").style.display = "block";
+            setTimeout(() =>
+            {
+                document.getElementById("mini_logo").style.left = "9vw";
+                document.getElementById("mini_logo").style.opacity = "100%";
+            }, 100);
+
+        }
+    }
+    if (containerTop > 0)
+    {
+        handlerFired = 0;
+        document.getElementById("mini_logo").style.left = "0vw";
+        document.getElementById("mini_logo").style.opacity = "0%";
+        document.getElementById("navbar").style.marginLeft = "0px";
+
+
+    }
+});
+
+
 function showText(id, delay) {
     var elem = document.getElementById(id);
-    setTimeout(function() {
+    setTimeout(function () {
         elem.style.opacity = 1;
     }, delay * 750)
 }
-window.onload = function() {
+window.onload = function () {
     showText('fade1', 7);
     showText('fade2', 8);
     showText('fade3', 9);
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
