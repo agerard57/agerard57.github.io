8998326024b3b314f9817565542ee31c838d01d5
agerard57 <56207146+agerard57@users.noreply.github.com>
2021-01-04 01:57:28 +0100

    Started working on the "projects"

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
 var handlerFired;
-window.addEventListener('scroll', function (e)
-{
+window.addEventListener('scroll', function (e) {
     var containerTop = document.querySelector('#logo').getBoundingClientRect().top;
-    if (containerTop <= 0)
-    {
-        if (!handlerFired)
-        {
+    if (containerTop <= 0) {
+        if (!handlerFired) {
             handlerFired = 1;
             console.log('container at top of viewport or above');
             document.getElementById("logo_animation").style.display = "block";
-            setTimeout(() =>
-            {
+            setTimeout(() => {
                 document.getElementById("mini_logo").style.left = "9vw";
                 document.getElementById("mini_logo").style.opacity = "100%";
             }, 100);
 
         }
     }
-    if (containerTop > 0)
-    {
+    if (containerTop > 0) {
         handlerFired = 0;
         document.getElementById("mini_logo").style.left = "0vw";
         document.getElementById("mini_logo").style.opacity = "0%";
@@ -44,4 +39,37 @@ window.onload = function () {
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+}
+
+//-----------------------------------Modal-----------------------------------
+
+
+var modaltitle = document.getElementById("modal-title");
+var modalcontent = document.getElementById("modal-text");
+var modallang = document.getElementById("modal-languages");
+
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+
+var btnc = document.getElementById("myBtn-C");
+
+btnc.onclick = function () {
+    modal.style.display = "block";
+    modaltitle.innerHTML = "Projet C"
+    modalcontent.innerHTML = "Mise en application des acquis dans le domaine de l'analyse et de la programmation dans un travail de groupe"
+    modallang.innerHTML = "Langage de programmation utilisée = C"
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
