5cbb99e3425ddffd6cfcd395aa1c1393c12f37d4
agerard57 <56207146+agerard57@users.noreply.github.com>
2021-01-09 14:51:55 +0100

    Fixed animation

--- js/footer.js
+++ js/footer.js
@@ -7,17 +7,17 @@ window.addEventListener('scroll', function (e) {
     if (containerTop <= 0) {
         if (!handlerFired1) {
             handlerFired1 = 1;
+            console.log('container at top of viewport or above');
             document.getElementById("logo_animation").style.display = "block";
             setTimeout(() => {
-                document.getElementById("mini_logo").style.left = "5vw";
+                document.getElementById("mini_logo").style.left = "5.5vw";
                 document.getElementById("mini_logo").style.opacity = "100%";
-
             }, 100);
 
         }
     }
-    if (cpt > 0) {
-        handlerFired2 = 0;
+    if (containerTop > 0) {
+        handlerFired1 = 0;
         document.getElementById("mini_logo").style.left = "0vw";
         document.getElementById("mini_logo").style.opacity = "0%";
         document.getElementById("navbar").style.marginLeft = "0px";
@@ -27,8 +27,6 @@ window.addEventListener('scroll', function (e) {
         if (!handlerFired2) {
             handlerFired2 = 1;
             document.getElementById('cpt').classList.add('animate');
-
-
         }
     }
 
