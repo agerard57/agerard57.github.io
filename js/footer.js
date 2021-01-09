d62c406411cfd15b6631e1be306ce81515db3ac8
agerard57 <56207146+agerard57@users.noreply.github.com>
2021-01-09 03:10:02 +0100

    Made things move, I guess ...

--- js/footer.js
+++ js/footer.js
@@ -1,24 +1,35 @@
-var handlerFired;
+var handlerFired1;
+var handlerFired2;
 window.addEventListener('scroll', function (e) {
     var containerTop = document.querySelector('#logo').getBoundingClientRect().top;
+    var cpt = document.querySelector('#pres').getBoundingClientRect().top;
+
     if (containerTop <= 0) {
-        if (!handlerFired) {
-            handlerFired = 1;
-            console.log('container at top of viewport or above');
+        if (!handlerFired1) {
+            handlerFired1 = 1;
             document.getElementById("logo_animation").style.display = "block";
             setTimeout(() => {
                 document.getElementById("mini_logo").style.left = "5vw";
                 document.getElementById("mini_logo").style.opacity = "100%";
+
             }, 100);
 
         }
     }
-    if (containerTop > 0) {
-        handlerFired = 0;
+    if (cpt > 0) {
+        handlerFired2 = 0;
         document.getElementById("mini_logo").style.left = "0vw";
         document.getElementById("mini_logo").style.opacity = "0%";
         document.getElementById("navbar").style.marginLeft = "0px";
-
-
     }
+
+    if (cpt <= 0) {
+        if (!handlerFired2) {
+            handlerFired2 = 1;
+            document.getElementById('cpt').classList.add('animate');
+
+
+        }
+    }
+
 });
--- js/modal.js
+++ js/modal.js
@@ -35,7 +35,7 @@ btnc.onclick = function () {
     modalcomp.innerHTML = "Maîtriser les concepts de l'algorithmique et de la programmation"
 
     modallang.innerHTML = "Langage C"
-    modalgit.src = "medias/img/proj/screens/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/JEU_DE_NIM"
 
     modalimg.src = "medias/img/proj/screens/m-c.png"
@@ -53,7 +53,7 @@ btnw1.onclick = function () {
     modalcomp.innerHTML = "- Aptitudes à synthétiser l’information écrite et sa présentation orale </br>- Maitrise de la recherche documentaire </br>- Aptitudes à la synthèse des compétences techniques acquises"
 
     modallang.innerHTML = "HTML5 - CSS3"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/Rap-n-Roll"
 
     modalimg.src = "medias/img/proj/screens/m-web1.png"
@@ -72,7 +72,7 @@ btnw2.onclick = function () {
     modalcomp.innerHTML = "- Mise en pratique de la méthodologie de conduite de projets </br>- Développement des compétences d’autonomie et d’initiative de l’étudiant </br>- Développement des aptitudes au travail en équipe"
 
     modallang.innerHTML = "HTML5 - CSS3 - JavaScript"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/AAEIUTM"
 
     modalimg.src = "medias/img/proj/screens/m-web2.png"
@@ -91,7 +91,7 @@ btnj.onclick = function () {
     modalcomp.innerHTML = "- Programmation par objet</br>- UML</br>- Langage Java (classe, héritage, gestion d'une interface graphique complète et gestion des événements graphiques…) "
 
     modallang.innerHTML = "Java"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/Videotheque_JAVA"
 
     modalimg.src = "medias/img/proj/screens/m-java.png"
@@ -110,7 +110,7 @@ btnasm.onclick = function () {
     modalcomp.innerHTML = "Maîtriser les concepts de fonctionnement d'un ordinateur"
 
     modallang.innerHTML = "Assembleur 8086"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/JEU_8086_ASM"
 
     modalimg.src = "medias/img/proj/screens/m-asm.jpg"
@@ -129,7 +129,7 @@ btnihm.onclick = function () {
     modalcomp.innerHTML = "- Maîtriser les concepts d'ergonomie et d'UX</br>- Gérer l'implémentation d'une BDD dans un projet"
 
     modallang.innerHTML = "Pascal"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/Projet-IHM"
 
     modalimg.src = "medias/img/proj/screens/m-ihm.png"
@@ -148,7 +148,7 @@ btncpoa.onclick = function () {
     modalcomp.innerHTML = "- Programmation événementielle</br>- JDBC</br>- Design Pattern DAO</br>- Architecture MVC</br>- Tests unitaires"
 
     modallang.innerHTML = "Java - JavaFX - JUnit - mySQL - Git"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/GERARD_GIANGRECO_CPOA_TD1_2020"
 
     modalimg.src = "medias/img/proj/screens/m-cpoa.png"
@@ -167,7 +167,7 @@ btnphp.onclick = function () {
     modalcomp.innerHTML = "- Maitrise de l'implémentation de la PHP</br>- Maitrise de l'implémentation de bases de données dans un projet"
 
     modallang.innerHTML = "PHP - CSS - JS"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/PHP_projet"
 
     modalimg.src = "medias/img/proj/screens/m-php.png"
@@ -186,7 +186,7 @@ btnga.onclick = function () {
     modalcomp.innerHTML = "- Maitrise de la création d'une page WEB (en particulier l'utilisation du JavaScript)</br>- Création d'un portefolio pour l'avenir"
 
     modallang.innerHTML = "HTML - CSS - JS"
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/agerard57.github"
 
     modalimg.src = "medias/img/proj/screens/GA_pp.jpg"
@@ -206,7 +206,7 @@ btnpd.onclick = function () {
 
     modallang.innerHTML = "ReactJS - TSX - SCSS - JavaScript - Json - FluentUI"
     
-    modalgit.src = "medias/img/proj/screens/git.png"
+    modalgit.src = "medias/img/logo/git.png"
     modallink.href = "https://github.com/agerard57/Pokedex-Front"
     modalimg.src = "medias/img/proj/screens/m-pd.png"
 
@@ -226,7 +226,7 @@ btnlu.onclick = function () {
 
     modallang.innerHTML = "C++ - C# - Python"
 
-    modalgit.src = "medias/img/proj/screens/fb.png"
+    modalgit.src = "medias/img/logo/fb.png"
     modallink.href = "https://www.facebook.com/LightUpCondorcet"
 
     modalimg.src = "medias/img/proj/screens/m-lu.png"
